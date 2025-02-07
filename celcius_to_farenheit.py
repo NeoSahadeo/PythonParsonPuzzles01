edfbdcfd4878155dd7d11c17361bd1daa5c3fd27
NeoSahadeo <neosahadeo@protonmail.com>
2025-02-07 14:51:13 +0000

    :rocket: more programs

---
+++ celcius_to_farenheit.py
@@ -0,0 +1,6 @@
+print(result)
+farenheit = celcius * 1.8 + 32
+def celcius_to_farenheit(celcius):
+temperature = float(input("Enter temperature: "))
+return farenheit
+result = celcius_to_farenheit(temperature)
